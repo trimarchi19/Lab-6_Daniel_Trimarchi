0c751e8fef9e71fb63027c8f5fc17f06fb2a31dd
trimarchi19 <dtrimarchi19@unitec.edu>
2018-06-08 13:21:27 -0600

    Creacion de las Clases

---
+++ Lab#6/src/lab/pkg6/Usuarios.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab.pkg6;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author X
+ */
+public class Usuarios {
+
+    private String contraseña;
+    private String numero;
+    private String correo;
+    private String fecha;
+    private ArrayList<Series> series=new ArrayList();
+
+    public Usuarios() {
+    }
+
+  
+
+
+}
